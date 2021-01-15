97cc3c4f51107ba3185f0b884479418934a979a6
irvian <irvian@cs.wallawalla.edu>
2021-01-15 01:19:44 +0000

    Commit homework/hw01 in-class/0108 in-class/0111

--- in-class/0111/activity02a.cpp
+++ in-class/0111/activity02a.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-  cout << 16 + 3 << endl;   
+ /* string schoolName = "Walla Walla ";
+  string schoolType = "University";
+  string fullName;
+  fullName = schoolName + schoolType;
+  cout << fullName << endl;
+
+  int age = 15;
+  cout << "Your age is: " << age << endl;
+  cout << 16 + 3 << endl;
   cout << 16 - 3 << endl;
-  cout << 16 * 3 << endl;   
-  cout << 16 / 3 << endl;   
-  cout << 16 % 3 << endl;   
+  cout << 16 * 3 << endl;
+  cout << 16 / 3 << endl;
+  cout << 16 % 3 << endl;*/
+
+cout << 4+3%2 << endl;
+cout << 3+9%2*(-1+3) << endl;
+
+  return (0);
 }
