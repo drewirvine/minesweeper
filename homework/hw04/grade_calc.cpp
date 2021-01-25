8737aeaeb2d598fb491b7b0501b1cb785cc17328
irvian <irvian@cs.wallawalla.edu>
2021-01-25 23:35:34 +0000

    Commit homework/hw03 homework/hw04

--- homework/hw04/grade_calc.cpp
+++ homework/hw04/grade_calc.cpp
@@ -15,10 +15,18 @@ using namespace std;
 int main() {
    
     // define variables
+    double TEST_AVERAGE;
+    double HOMEWORK_AVERAGE;
+    double PART_AVERAGE;
+    double 
+
 
     // gather input
+    cout << "Enter your test, homework, and participation averages separated by spaces: ";
+    cin >> TEST_AVERAGE >> HOMEWORK_AVERAGE >> PART_AVERAGE;
 
     // validate that input
+
 
     // branching statements to produce output
 
