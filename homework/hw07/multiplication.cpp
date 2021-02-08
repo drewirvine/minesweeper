7a7a622bdfd840ebf87722329c4f2b40edf8a82a
irvian <irvian@cs.wallawalla.edu>
2021-02-08 23:41:45 +0000

    Commit homework/hw07

--- homework/hw07/multiplication.cpp
+++ homework/hw07/multiplication.cpp
@@ -36,30 +36,26 @@ int main() {
   // Header
 
   for (int i = 0; i < userColumns + 2; i++) {
-      if(i == 0) {
-          cout << setw(6) << "*  ";
-          for (int j = 0; j < userColumns; j++) {
-            cout << setw(4) << j + 1;
-          }
-          cout << endl;
+    if (i == 0) {
+      cout << setw(6) << "*  ";
+      for (int j = 0; j < userColumns; j++) {
+        cout << setw(4) << j + 1;
       }
-      else if(i == 1) {
-          cout << setw(6) << "____";
-          for(int j = 0; j < userColumns; j++) {
-              cout << setw(4) << "___";
-          }
-          cout << endl;
+      cout << endl;
+    } else if (i == 1) {
+      cout << setw(6) << "____";
+      for (int j = 0; j < userColumns; j++) {
+        cout << setw(4) << "___";
       }
-      else {
-          cout << setw(3) << " " << (i-1) << " |";
-          for (int j = 0; j < userColumns; j++) {
-              cout << setw(4) << (i-1) * (j + 1);
-          }
-          cout << endl;
-
+      cout << endl;
+    } else {
+      cout << setw(3) << " " << (i - 1) << " |";
+      for (int j = 0; j < userColumns; j++) {
+        cout << setw(4) << (i - 1) * (j + 1);
       }
+      cout << endl;
+    }
   }
-  
 
   cout << endl;
   return 0;
