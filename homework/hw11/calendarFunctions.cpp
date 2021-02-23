0c089eae78830c59267abd25e8316d6f2d9d1f80
irvian <irvian@cs.wallawalla.edu>
2021-02-23 03:43:55 +0000

    Commit homework/hw11

--- homework/hw11/calendarFunctions.cpp
+++ homework/hw11/calendarFunctions.cpp
@@ -8,34 +8,80 @@
  *
  */
 
-#include <iomanip>
-#include <string>
-#include <iostream>
 #include "calendarFunctions.h"
+#include <iomanip>
+#include <iostream>
+#include <string>
 using namespace std;
 
-string nameOfMonth (int month) {
-    string monthName;
-    month = 0;
-    cout << "Enter in month name: ";
-    cin >> monthName;
-
-return monthName;
+string nameOfMonth(int month) {
+  string str;
+  switch (month) {
+  case 1:
+    str = "January";
+    break;
+  case 2:
+    str = "February";
+  case 3:
+    str = "March";
+    break;
+  case 4:
+    str = "April";
+    break;
+  case 5:
+    str = "May";
+    break;
+  case 6:
+    str = "June";
+    break;
+  case 7:
+    str = "July";
+    break;
+  case 8:
+    str = "August";
+    break;
+  case 9:
+    str = "September";
+    break;
+  case 10:
+    str = "October";
+    break;
+  case 11:
+    str = "November";
+    break;
+  case 12:
+    str = "December";
+    break;
+  }
+  return str;
 }
 
-bool isLeapYear (int year) {
-    bool leapYear = false;
-    cout << "Enter Year";
-    cin >> year;
-    if(year )
+bool isLeapYear(int year) {
+  bool leapYear = false;
+  if (year % 4 == 0) {
+    leapYear = true;
+  } else if ((year % 100 == 0) && !(year % 400 == 0)) {
+    leapYear = false;
+  }
 
-    return leapYear;
+  return leapYear;
 }
 
 int daysInMonthAndYear(int month, int year) {
-    int days
+  int days;
+  if ((month == 1) || (month == 3) || (month == 5) || (month == 7) ||
+      (month == 8) || (month == 10) || (month == 12)) {
+    days = 31;
+  } else if ((month == 4) || (month == 6) || (month == 9) || (month == 11)) {
+    days = 30;
+  } else if (month == 2) {
+    if (isLeapYear(year)) {
+      days = 29;
+    }
+    days = 28;
+  }
 
-    return 
+  return days;
 }
 
 // implement your functions here
