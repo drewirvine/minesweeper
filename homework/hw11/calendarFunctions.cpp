f6a3e600d06c18c771a468c7c9e38bd48d12fa38
irvian <irvian@cs.wallawalla.edu>
2021-02-23 00:17:32 +0000

    Commit homework/hw10 homework/hw11 in-class/0219

--- homework/hw11/calendarFunctions.cpp
+++ homework/hw11/calendarFunctions.cpp
@@ -8,9 +8,34 @@
  *
  */
 
+#include <iomanip>
 #include <string>
+#include <iostream>
+#include "calendarFunctions.h"
 using namespace std;
 
-#include "calendarFunctions.h"
+string nameOfMonth (int month) {
+    string monthName;
+    month = 0;
+    cout << "Enter in month name: ";
+    cin >> monthName;
+
+return monthName;
+}
+
+bool isLeapYear (int year) {
+    bool leapYear = false;
+    cout << "Enter Year";
+    cin >> year;
+    if(year )
+
+    return leapYear;
+}
+
+int daysInMonthAndYear(int month, int year) {
+    int days
+
+    return 
+}
 
 // implement your functions here
