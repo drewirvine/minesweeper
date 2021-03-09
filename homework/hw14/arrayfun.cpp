d2f1d6892bd9fe4099434d88bf1efda7047f1abe
irvian <irvian@cs.wallawalla.edu>
2021-03-09 04:05:27 +0000

    Commit homework/hw14

--- homework/hw14/arrayfun.cpp
+++ homework/hw14/arrayfun.cpp
@@ -10,4 +10,89 @@
  */
 
 #include "arrayfun.h"
+#include <iostream>
 
+using namespace std;
+
+double mean(int a[], int size) { // average = sum / count
+  double sum;
+  for (int i = 0; i < size; i++) {
+    sum += a[i];
+  }
+  return sum / (double)size;
+}
+
+double median(int a[], int size) { // Middle value
+
+  int indexSmallest = 0;
+  int temp = 0; // Temporary variable for swap
+
+  for (int i = 0; i < size - 1; ++i) {
+
+    // Find index of smallest remaining element
+    indexSmallest = i;
+    for (int j = i + 1; j < size; ++j) {
+
+      if (a[j] < a[indexSmallest]) {
+        indexSmallest = j;
+      }
+    }
+
+    // Swap numbers[i] and numbers[indexSmallest]
+    temp = a[i];
+    a[i] = a[indexSmallest];
+    a[indexSmallest] = temp;
+  }
+  int index = (size + 1) / 2;
+  return a[index];
+}
+
+// void SelectionSort(int numbers[], int numbersSize) {
+//   int indexSmallest = 0;
+//   int temp = 0; // Temporary variable for swap
+
+//   for (int i = 0; i < numbersSize - 1; ++i) {
+
+//     // Find index of smallest remaining element
+//     indexSmallest = i;
+//     for (int j = i + 1; j < numbersSize; ++j) {
+
+//       if (numbers[j] < numbers[indexSmallest]) {
+//         indexSmallest = j;
+//       }
+//     }
+
+// // Swap numbers[i] and numbers[indexSmallest]
+// temp = numbers[i];
+// numbers[i] = numbers[indexSmallest];
+// numbers[indexSmallest] = temp;
+// }
+// }
+
+// int main2() {
+//    int numbers[] = { 10, 2, 78, 4, 45, 32, 7, 11 };
+//    const NUMBERS_SIZE = 8;
+
+//    void print("UNSORTED: ");
+//    for ( i = 0; i < NUMBERS_SIZE; ++i) {
+//       print(numbers[i] + " ");
+//    }
+//    void printLine();
+
+//    SelectionSort(numbers, NUMBERS_SIZE);
+
+//    print("SORTED: ");
+//    for ( i = 0; i < NUMBERS_SIZE; ++i) {
+//       print(numbers[i] + " ");
+//    }
+//    printLine();
+// }
+
+int main() {
+  int a[]{1, 2, 3, 4, 5, 6};
+  cout << "The average is: " << mean(a, 6);
+  cout << endl;
+
+  cout << "The median is: " << median(a, 6);
+  cout << endl;
+}
