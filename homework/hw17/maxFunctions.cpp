8ba89e0ec61ee109baba4582231609007fd8835c
irvian <irvian@cs.wallawalla.edu>
2021-03-17 02:04:13 +0000

    Commit homework/hw17

--- homework/hw17/maxFunctions.cpp
+++ homework/hw17/maxFunctions.cpp
@@ -7,4 +7,27 @@
  */
 
 #include "maxFunctions.h"
+#include <sstream>
 using namespace std;
+
+int findMax(string maxFind) {
+  int maxValue = 0;
+  for (int i = 0; i < maxFind.size(); i++) {
+    string temp(1, maxFind[i]);
+
+    stringstream str(temp);
+    int num;
+    str >> num;
+    if (maxValue < num) {
+      maxValue = num;
+    }
+  }
+  return maxValue;
+}
+
+string getSet(const int array[], int num) {
+  string mainString = "{ ";
+  for (int i = 0; i < sizeof(array); i++) {
+    string mainString += num;
+  }
+}
